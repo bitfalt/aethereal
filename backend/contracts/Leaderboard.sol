091279ede7e3d8f25ae4d4c1050bc3c62c4bf16e
bitfalt <75431447+bitfalt@users.noreply.github.com>
2024-09-08 02:07:20 -0600

    Update Leaderboard contract

--- backend/contracts/Leaderboard.sol
+++ backend/contracts/Leaderboard.sol
@@ -3,14 +3,11 @@ pragma solidity ^0.8.20;
 
 contract Leaderboard {
 
-  // person who deploys contract is the owner
-  address owner;
-
   // lists top 5 users
-  uint leaderboardLength = 5;
+  uint8 private leaderboardLength = 5;
 
   // create an array of Users
-  mapping (uint => User) public leaderboard;
+  mapping (uint8 => User) public leaderboard;
     
   // each user has an address and score
     struct User {
@@ -22,13 +19,13 @@ contract Leaderboard {
   function updateUserScore(address userAddress, uint score) public {
 
     // loop through the leaderboard
-    for (uint i=0; i<leaderboardLength; i++) {
+    for (uint8 i=0; i<leaderboardLength; i++) {
       // find where to insert the new score
       if (leaderboard[i].score < score) {
 
         // shift leaderboard
         User memory currentUser = leaderboard[i];
-        for (uint j=i+1; j<leaderboardLength+1; j++) {
+        for (uint8 j=i+1; j<leaderboardLength+1; j++) {
           User memory nextUser = leaderboard[j];
           leaderboard[j] = currentUser;
           currentUser = nextUser;
@@ -46,8 +43,8 @@ contract Leaderboard {
     }
   }
 
-  function getUserScore(address userAddress) public view returns (uint) {
-    for (uint i=0; i<leaderboardLength; i++) {
+  function getUserScore(address userAddress) public view returns (uint256) {
+    for (uint8 i=0; i<leaderboardLength; i++) {
       if (leaderboard[i].userAddress == userAddress) {
         return leaderboard[i].score;
       }
